3b2bc74d1669adfa7e18c21d0a9492470b3b2eed
Turco777 <agusalmona@gmail.com>
2023-07-25 19:30:56 -0300

    creando server

--- serverConExpress.js
+++ serverConExpress.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express()
+const express = require("express");
+const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
